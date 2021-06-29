702bb065be7aefe9e48e7ef7624d51f0184716a7
jroberto <jrobertoonb@gmail.com>
2021-06-28 23:24:12 -0300

    chore: alterando a estrutura do projeto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
-
-import './components';
+import router from './router';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
-}).$mount('#root');
+}).$mount('#app');
