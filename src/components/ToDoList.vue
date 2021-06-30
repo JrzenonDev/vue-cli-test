11b6bf938c8e04e7b8ddd4663d676a46ef5594f6
jroberto <jrobertoonb@gmail.com>
2021-06-29 21:46:20 -0300

    chore: criando uma todo-list, utilizando to way data bind do v-model, utilizando trim

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>To-Do List With Vue</h1>
+  <div>
+    <h1>To-Do List With Vue</h1>
+    <input v-model.trim="task">
+    <button @click="addTask" type="button">Adicionar</button>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{task}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ToDoList',
+  data() {
+    return {
+      task: 'José',
+      tasks: ['Task 01', 'Task 02', 'Task 03'],
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.task);
+      this.task = '';
+    },
+  },
 };
 </script>
