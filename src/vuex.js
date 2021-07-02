8c13b67c9e53ca4f5bd170efcde3c7097c252ccc
JrzenonDev <jrobertoonb@gmail.com>
2021-07-01 23:13:13 -0300

    chore: utilizando getters no vuex

--- src/vuex.js
+++ src/vuex.js
@@ -5,11 +5,14 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    counter: 100,
+    counter: 0,
   },
   mutations: {
     decrement: state => state.counter -= 1,
     increment: state => state.counter += 1,
+  },
+  getters: {
+    counter: state => state.counter,
   },
 });
 
